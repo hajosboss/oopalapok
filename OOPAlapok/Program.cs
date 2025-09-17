e49f957f7728ae6bedb4acc7850cf828dc695e86
FZoltan40 <farkasz@kkszki.hu>
2025-09-17 12:19:39 +0200

    Feladat is ready.

--- OOPAlapok/Program.cs
+++ OOPAlapok/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOPAlapok
 {
+    public class Szemely
+    {
+        public string _nev = "Nagy Ilona";
+        public int _kor = 35;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely tanulo1 = new Szemely();
+            Console.WriteLine(tanulo1._nev);
+            Console.WriteLine(tanulo1._kor);
         }
     }
 }
