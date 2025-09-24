b7d86b67c8612a5e1af938126abed8ae19096d26
FZoltan40 <farkasz@kkszki.hu>
2025-09-24 12:06:23 +0200

    Feladat3 is ready to use.

--- OOPAlapok/Program.cs
+++ OOPAlapok/Program.cs
@@ -7,24 +7,39 @@ namespace OOPAlapok
         private string _nev;
         private int _kor;
 
-        public Szemely(string nev, int kor)
+        public Szemely(string nev)
         {
             _nev = nev;
-            _kor = kor;
+
         }
 
-        public string Kiir()
+        public string Nev
         {
-            return _nev + _kor;
-
+            get { return _nev; }
+            //set { _nev = value; }
         }
+
+        public int Kor
+        {
+            get { return _kor; }
+            set
+            {
+                if (value >= 0)
+                    _kor = value;
+                else
+                    Console.WriteLine("Hibás érték.");
+
+            }
+        }
+
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Szemely tanulo1 = new Szemely("Kiss Ilona", 44);
-            Console.WriteLine(tanulo1.Kiir());
+            Szemely tanulo1 = new Szemely("Kiss Ilona");
+            tanulo1.Kor = 34;
+            Console.WriteLine(tanulo1.Nev + tanulo1.Kor);
         }
     }
 }
